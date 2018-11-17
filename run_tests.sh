c5b40b702f601621555fb8d7d812e716f0812280
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-11-17 11:06:03 +0900

    Fix test script.

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 find . -mindepth 2 -name "ex*.ml" | while read line;
   do
     if [ `echo $line | grep 'metro'` ]; then
-      ocamlc metro.ml $line && ./a.out
+      ocamlc -o a.out metro.ml $line && ./a.out
     else
       ocaml $line
     fi
